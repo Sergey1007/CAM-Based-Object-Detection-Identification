7c2ffe59b5b2c0e499e058f8803f9ad43c341171
tkac0 <arcan0n@protonmail.com>
2022-07-04 06:01:24 -0700

    added i2c_block

---
+++ soft/python/slava_vision/i2c_block.py
@@ -0,0 +1,24 @@
+import smbus
+import time
+
+bus=smbus.SMBus(1)
+SLAVE_ADDRESS=0x04
+
+def request_reading():
+	reading=int(bus.read_byte(SLAVE_ADDRESS))
+	print(reading)
+
+def send_symbol(symbol):
+	bus.write_byte(SLAVE_ADDRESS,symbol)
+
+def send_arr(data):
+	bus.write_i2c_block_data(SLAVE_ADDRESS,data[0],data[1:])
+
+if  __name__=='__main__':
+	data=[]
+	for _ in range (4):
+		input(int(symbol))
+		data.append(symbol)
+	print(data)
+	send_arr(data)
+
