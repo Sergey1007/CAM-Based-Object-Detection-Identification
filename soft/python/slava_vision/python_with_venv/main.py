eb21f966ffe9c3b5ee61ddf3af3ee997bd0dfd44
tkac0 <arcan0n@protonmail.com>
2022-07-06 13:29:54 +0300

    added exception for start point

--- soft/python/slava_vision/python_with_venv/main.py
+++ soft/python/slava_vision/python_with_venv/main.py
@@ -8,17 +8,34 @@ old_coordinate=[]
 new_coordinate=[]
 diff=[0,0]
 
-#img = cv2.imread('sources/laser3.jpg')
-cap = cv2.VideoCapture(0)
-_, img = cap.read()
-old_coordinate=st.get_start_xy(img)
 
-print(old_coordinate)
-cv2.circle(img, old_coordinate, 10, (255, 0, 0), -1) #you can draw a found point
-cv2.imshow('result',img)
-cv2.waitKey(5000)
+a=0
+while (old_coordinate==[]):
+    if (a==0):
+        img = cv2.imread('sources/ipad1.jpg')
+
+    if (a==1):
+        img = cv2.imread('sources/laser3.jpg')
+    a+=1
 
 
+    #cap = cv2.VideoCapture(0)
+    #_, img = cap.read()
+    try:
+        old_coordinate=st.get_start_xy(img)
+    except ZeroDivisionError:
+        print('error')
+        cv2.imshow('result', img)
+        cv2.waitKey(1000)
+    else:
+        print(old_coordinate)
+        cv2.circle(img, old_coordinate, 10, (255, 0, 0), -1) #you can draw a found point
+        cv2.imshow('result',img)
+        cv2.waitKey(1000)
+print('cycle exit')
+
+
+'''
 img = cv2.imread('sources/ipad1.jpg')
 new_coordinate=get_red_xy(img)
 print(new_coordinate)
@@ -28,5 +45,5 @@ cv2.waitKey(5000)
 diff[0]=new_coordinate[0]-old_coordinate[0]
 diff[1]=new_coordinate[1]-old_coordinate[1]
 print(diff)
-
+'''
 #print(diff)
