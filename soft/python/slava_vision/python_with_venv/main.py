63b86f4afc4ef8f42e1f7c4813fc6c2e4214c67a
tkac0 <arcan0n@protonmail.com>
2022-07-12 03:18:07 -0700

    solve errors of signal

--- soft/python/slava_vision/python_with_venv/main.py
+++ soft/python/slava_vision/python_with_venv/main.py
@@ -2,13 +2,13 @@ import get_start_point as st
 import cv2
 import numpy as np
 #from matplotlib import pyplot
-from color_detection import get_red_xy
+#from color_detection import get_red_xy
 from signal import make_signal
 
 
 
 old_coordinate=[]
-new_coordinate=[920/2,1280/2]
+new_coordinate=[1280/2,920/2]
 
 a=0
 while (old_coordinate==[]):
@@ -27,13 +27,14 @@ while (old_coordinate==[]):
         old_coordinate=st.get_start_xy(img)
     except ZeroDivisionError:
         print('error')
-        #cv2.imshow('result', img)
-        #cv2.waitKey(3000)
+        cv2.imshow('result', img)
+        cv2.waitKey(3000)
     else:
         #print(old_coordinate)
         cv2.circle(img, old_coordinate, 10, (255, 0, 0), -1) #you can draw a found point
-        #cv2.imshow('result',img)
-        #cv2.waitKey(3000)
+        cv2.imshow('result',img)
+        cv2.waitKey(3000)
+print(old_coordinate)
 print('cycle exit')
 signal=make_signal(old_coordinate,new_coordinate)
 print(signal)
--- soft/python/slava_vision/python_with_venv/signal.py
+++ soft/python/slava_vision/python_with_venv/signal.py
@@ -5,16 +5,16 @@ def make_signal(old_c,new_c):
     dif[1] = new_c[1]- old_c[1]
 
     if (dif[0]<0):  #set directions
-        signal[5]=0
-    if (dif[1]<0):
         signal[2]=0
+    if (dif[1]<0):
+        signal[5]=0
 
     step=1.8
     verticale_angle=41  #set steps
     horizontal_angle=54
     horizontal_koef=1280/horizontal_angle
-    vetical_koef=720/verticale_angle
-    signal[1]=int(abs(dif[1]/horizontal_koef/step)) #steps for motor 1
-    signal[4] = int(abs(dif[0] / vetical_koef/step))  # steps for motor 1
+    vetical_koef=920/verticale_angle
+    signal[1]=int(abs(dif[0]/horizontal_koef/step)) #steps for motor 1
+    signal[4] = int(abs(dif[1] / vetical_koef/step))  # steps for motor 1
 
     return signal
